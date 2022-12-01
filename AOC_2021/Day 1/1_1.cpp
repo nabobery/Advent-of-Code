ee9aaa995c182689a5fa3eba413bc15c78153b88
Avinash Changrani <74554800+nabobery@users.noreply.github.com>
2022-12-01 11:43:35 +0530

    Added AOC 2021 Day 1

---
+++ AOC_2021/Day 1/1_1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<fstream>
+#include <vector>
+using namespace std;
+
+int main(){
+    string filename("input.txt");
+    vector<int> numbers;
+    int number;
+    ifstream input_file(filename);
+    if (!input_file.is_open()) {
+        cerr << "Could not open the file - '" << filename << "'" << endl;
+        return EXIT_FAILURE;
+    }
+
+    while (input_file >> number) {
+        numbers.push_back(number);
+    }
+    int result = 0;
+    for(int i = 1; i < numbers.size(); i++) {
+        if(numbers[i] > numbers[i - 1]){
+            result++;
+        }
+    }
+    cout << result<<"\n";
+    input_file.close();
+
+    return EXIT_SUCCESS;
+}
